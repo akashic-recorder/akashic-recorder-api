4eddd68f184897448291a1f37be60ff71cd27698
knagato <nagato.kasaki@gmail.com>
2022-03-09 23:01:42 +0900

    add cors

--- pages/api/[gameid]/80001/[address]/index.ts
+++ pages/api/[gameid]/80001/[address]/index.ts
@@ -1,22 +1,42 @@
+import Cors from 'cors'
+import initMiddleware from '../../../../../lib/init-middleware'
 import { NextApiRequest, NextApiResponse } from 'next'
 import { sampleEventData } from '../../../../../utils/sample-event'
 
-const handler = (req: NextApiRequest, res: NextApiResponse) => {
-  const { gameid, chainid, address } = req.query
-  const resData = sampleEventData.map(e => {
-    return {
-      ...e,
-      address: address
-    }
+const cors = initMiddleware(
+  Cors({
+    methods: ['GET', 'POST', 'OPTIONS'],
   })
-  try {
-    if (!Array.isArray(resData)) {
-      throw new Error('Cannot find user data')
-    }
+)
 
-    res.status(200).json(resData)
-  } catch (err: any) {
-    res.status(500).json({ statusCode: 500, message: err.message })
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  const { query: {gameid, chainid, address }, method } = req
+  await cors(req, res)
+
+  switch (method) {
+    case 'GET':
+      const resData = sampleEventData.map(e => {
+        return {
+          ...e,
+          address: address
+        }
+      })
+      try {
+        if (!Array.isArray(resData)) {
+          throw new Error('Cannot find user data')
+        }
+        res.status(200).json(resData)
+      } catch (err: any) {
+        res.status(500).json({ statusCode: 500, message: err.message })
+      }
+      break
+    case 'POST':
+      // Update or create data in your database
+      res.status(200).json({})
+      break
+    default:
+      res.setHeader('Allow', ['GET', 'POST'])
+      res.status(405).end(`Method ${method} Not Allowed`)
   }
 }
 
